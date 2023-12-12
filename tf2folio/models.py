38530a5c832d42f70ca82bc075dd56710b0a1c9a
Henrik <Henriko99@live.no>
2023-12-12 13:43:29 +0100

    added models, implement user authentication (login and register), and create basic templates for login/logout/register and index.

--- tf2folio/models.py
+++ tf2folio/models.py
@@ -1,3 +1,63 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    items = models.ManyToManyField('Item', blank=True, related_name="item_owner")
+    transactions = models.ManyToManyField('Transaction', blank=True, related_name="transaction_owner")
+
+class Item(models.Model):
+
+    QUALITY = [
+        ('unique', 'Unique'),
+        ('strange', 'Strange'),
+        ('vintage', 'Vintage'),
+        ('unusual', 'Unusual'),
+        ('genuine', 'Genuine'),
+        ('decorated', 'Decorated'),
+        ('haunted', 'Haunted'),
+        ('collectors', 'Collectors'),
+        ('normal', 'Normal'),
+    ]
+
+    WEAR_TIERS = [
+        ('factory_new', 'Factory New'),
+        ('minimal_wear','Minimal Wear'),
+        ('field_tested','Field-Tested'),
+        ('well_worn','Well-Worn'),
+        ('battle_scarred','Battle Scarred')
+    ]
+
+    KILLSTREAK_TIERS = [
+        ('standard', 'Standard'),
+        ('specialized', 'Specialized'),
+        ('professional', 'Professional')
+    ]
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings", default=None)
+    quality = models.CharField(max_length=20, choices=QUALITY)
+    item_name = models.CharField(max_length=150)
+    craftable = models.BooleanField(default=True)
+    australium = models.BooleanField(default=False)
+    texture_name = models.CharField(max_length=64, null=True, blank=True)
+    wear = models.CharField(max_length=20, choices=WEAR_TIERS)
+    particle_effect = models.CharField(max_length=64)
+    description = models.TextField(max_length=400, null=True, blank=True)
+    killstreak = models.CharField(max_length=20, choices=KILLSTREAK_TIERS, null=True, blank=True)
+    # date_arrived = models.DateTimeField(null=True, blank=True)
+    # date_sold = models.DateTimeField(null=True, blank=True)
+    image_url = models.URLField(max_length=400, null=True, blank=True)
+
+
+class Transaction(models.Model):
+    TRANSACTION_CHOICES = [
+        ('buy', 'Buy'),
+        ('sale', 'Sale')
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "user_transactions", default=None)
+    transaction_type = models.CharField(max_length=4, choices=TRANSACTION_CHOICES)
+    items_sold = models.ManyToManyField('Item', blank=True, related_name="sales_transactions")
+    items_bought = models.ManyToManyField('Item', blank=True, related_name="buys_transactions")
+    description = models.TextField(max_length=400, null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
--- tf2folio/views.py
+++ tf2folio/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db import IntegrityError
+from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, "tf2folio/index.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "tf2folio/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "tf2folio/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username =request.POST["username"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "tf2folio/register.html", {
+                "message": "Passwords must match."
+            })
+
+        try: 
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+        except IntegrityError:
+            return render(request, "tf2folio/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "tf2folio/register.html")
+
+    
+    
+
+
